090df7510c57b574796baf548ed1e445ae9a580d
sandy1988302 <70570253+sandy1988302@users.noreply.github.com>
2020-09-02 17:22:13 +0800

    9.2 update com

--- demo/src/main/java/com/example/demo/SleepDemo.java
+++ demo/src/main/java/com/example/demo/SleepDemo.java
@@ -1,14 +1,16 @@
 package com.example.demo;
+
 import java.util.*;
-  
+
 public class SleepDemo {
-   public static void main(final String args[]) {
-      try { 
-         System.out.println(new Date( ) + "\n"); 
-         Thread.sleep(1000*3);   // 休眠3秒
-         System.out.println(new Date( ) + "\n"); 
-      } catch (final Exception e) { 
-          System.out.println("Got an exception!"); 
-      }
-   }
+
+  public static void main(final String args[]) {
+    try {
+      System.out.println(new Date() + "\n");
+      Thread.sleep(1000 * 3); // 休眠3秒
+      System.out.println(new Date() + "\n");
+    } catch (final Exception e) {
+      System.out.println("Got an exception!");
+    }
+  }
 }
